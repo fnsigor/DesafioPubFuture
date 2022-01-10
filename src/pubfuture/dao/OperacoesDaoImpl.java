27d7f2b6edb367d456fe0631932d254cf0713ca6
fnsigor <igorfernamdez@gmail.com>
2022-01-10 19:24:53 -0300

    Transferencia funcionando

--- src/pubfuture/dao/OperacoesDaoImpl.java
+++ src/pubfuture/dao/OperacoesDaoImpl.java
@@ -6,11 +6,11 @@
 package pubfuture.dao;
 
 import java.sql.Connection;
-import java.sql.Date;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import pubfuture.bean.Conta;
-import pubfuture.bean.Receitas;
+import pubfuture.bean.Despesas;
 
 /**
  *
@@ -18,6 +18,46 @@ import pubfuture.bean.Receitas;
  */
 public class OperacoesDaoImpl {
 
-   
+    private Connection connection;
+    private PreparedStatement psDevedor;
+    private PreparedStatement psRecebedor;
+    private String sqlRecebedor;
+    private String sqlDevedor;
+    private Despesas despesa;
+    private ContaDaoImpl contaDao;
+
+    public void transferencia(int idPagador, int idRecebedor, double valorTransferencia) {
+
+        sqlDevedor = "UPDATE CONTA SET SALDO=? WHERE IDCONTA=?";
+        sqlRecebedor = "UPDATE CONTA SET SALDO=? WHERE IDCONTA=?";
+
+        try {
+
+            contaDao = new ContaDaoImpl();
+
+            connection = ConnectionFactory.abreConexao();
+            psDevedor = connection.prepareStatement(sqlDevedor);
+            psRecebedor = connection.prepareStatement(sqlRecebedor);
+
+            Conta pagador = contaDao.pesquisaPorId(idPagador);
+            Conta recebedor = contaDao.pesquisaPorId(idRecebedor);
+
+            double novoSaldoPagador = pagador.getSaldo() - valorTransferencia;
+            double novoSaldoRecebedor = recebedor.getSaldo() + valorTransferencia;
+
+            psDevedor.setDouble(1, novoSaldoPagador);
+            psDevedor.setInt(2, idPagador);
+            psRecebedor.setDouble(1, novoSaldoRecebedor);
+            psRecebedor.setInt(2, idRecebedor);
+
+            psDevedor.executeUpdate();
+            psRecebedor.executeUpdate();
+
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+        }
+
+    }
 
 }
---
+++ test/pubfuture/dao/OperacoesDaoImplTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pubfuture.dao;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Igor Fernandes
+ */
+public class OperacoesDaoImplTest {
+    
+    OperacoesDaoImpl operacao = new OperacoesDaoImpl();
+    
+    public OperacoesDaoImplTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        operacao.transferencia(1, 2, 20.0);
+    }
+    
+}
